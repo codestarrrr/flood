8288395336b7491dd5d316f91a66a8dbb4b02bfd
codestarrrr <52021386+codestarrrr@users.noreply.github.com>
2020-11-28 15:31:30 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,17 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
+//development connection
+// $db_host		= 'localhost';
+// $db_user		= 'u899058486_flood';
+// $db_pass		= 'serenity15';
+// $db_database	= 'u899058486_flood'; 
 
-$db_host		= 'localhost';
-$db_user		= 'u899058486_flood';
-$db_pass		= 'serenity15';
-$db_database	= 'u899058486_flood'; 
+//remote database connection
+$db_host		= 'remotemysql.com';
+$db_user		= 'POgKk335ek';
+$db_pass		= 'UDdlG5FYNy';
+$db_database	= 'POgKk335ek'; 
+
 
 $link = mysql_connect($db_host,$db_user,$db_pass) or die('Unable to establish a DB connection');
 
