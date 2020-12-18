5e4516002ad99f293ebb5e24c3b50e12089d5829
codestarrrr <52021386+codestarrrr@users.noreply.github.com>
2020-12-19 04:27:59 +0800

    Update rexec.php

--- rexec.php
+++ rexec.php
@@ -7,14 +7,14 @@ $address= $_POST['address'];
 $contact=  '0'.$_POST['contact'];
 $type= '';
 
-$result = mysql_query("SELECT * FROM POgKk335ek.login2 WHERE contact = '$contact'");
+$result = mysql_query("SELECT * FROM login2 WHERE contact = '$contact'");
 $count=mysql_num_rows($result);
 if ($count > 0 ) {
 	echo '<script>alert("Mobile Number already registered.");window.location.href = \'index.php\';</script>';
 } else {
 	echo 'a';
-	$save1=mysql_query("INSERT INTO POgKk335ek.login2 (name,barangay, contact,type) VALUES ('$name','$address','$contact','$type')");
-	$save1=mysql_query("INSERT INTO POgKk335ek.login3 (username,password,type1) VALUES ('$username','$password','$type')");
+	$save1=mysql_query("INSERT INTO login2 (name,barangay, contact,type) VALUES ('$name','$address','$contact','$type')");
+	$save1=mysql_query("INSERT INTO login3 (username,password,type1) VALUES ('$username','$password','$type')");
 	
 echo "<script type=\"text/javascript\">window.alert('You have been registered. You can now enter your login information.');window.location.href = 'index.php';</script>"; 
 }
